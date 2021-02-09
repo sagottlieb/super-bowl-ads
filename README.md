c65671399aad818d6a80ad9eb7572e4df07d8d70
Sam <sagottlieb@gmail.com>
2021-02-09 11:16:39 -0500

    Adding implementation I used a few months back to retrieve data for 2018, 2019, and 2020

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# super-bowl-ads
+# Super Bowl Ads
+
+A friend asked me to scrape data on super bowl ads from this site
+https://admeter.usatoday.com/results/2020 for 2020, 2019, and 2018.
+
+Desired output is a csv file for each year with columns
+
+Year,Brand,Ad Name,Rank,Avg. Rating,Air Time,Link
+
+where link is to admeter detail page for the ad (e.g. [https://admeter.usatoday.com/commercials/cashew/](https://admeter.usatoday.com/commercials/cashew/)).
---
+++ scrape.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import csv
+
+year = 2018
+url = 'https://admeter.usatoday.com/results/'
+
+page = urlopen('%s%d' % (url, year))
+html = page.read().decode("utf-8")
+soup = BeautifulSoup(html, "html.parser")
+content = soup.find(id='content')
+
+with open('%d.csv' % year, 'w') as csvfile:
+
+    csvwriter = csv.writer(csvfile)
+
+    for index, entry in enumerate(content.find_all('article', class_='ranking')):    
+        link = entry.find('a', class_='ranking_link')['href']
+        
+        ranking_parts = entry.find('dl').find_all('dd')
+        [avg_ranking, air_time] = [x.text for x in ranking_parts]
+
+        title = entry.find('h2', class_='ranking_title').text.strip()
+
+        brand = entry.find('span', class_='ranking_advertiser').text
+
+        csvwriter.writerow([year, brand, title, index+1, avg_ranking, air_time, link])    
