fb0cd64da09cddb09a8608409a8589bd78c7f983
Sam <sagottlieb@gmail.com>
2021-02-09 13:01:46 -0500

    Website has been redesigned since last time I pulled data, so revamp data extraction

--- scrape.py
+++ scrape.py
@@ -4,26 +4,25 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-year = 2018
+year = 2021
 url = 'https://admeter.usatoday.com/results/'
 uri = '%s%d' % (url, year)
 
 response = requests.get(uri)
 soup = BeautifulSoup(response.text, "html.parser")
-content = soup.find(id='content')
+commercials = soup.find(id='commercials-list')
 
 with open('%d.csv' % year, 'w') as csvfile:
 
     csvwriter = csv.writer(csvfile)
 
-    for index, entry in enumerate(content.find_all('article', class_='ranking')):    
-        link = entry.find('a', class_='ranking_link')['href']
-        
-        ranking_parts = entry.find('dl').find_all('dd')
-        [avg_ranking, air_time] = [x.text for x in ranking_parts]
+    for index, entry in enumerate(commercials.find_all('article', class_='commercial-block collapsible-block filterable__item commercial-block--results')):    
+        link = entry.find('a', class_='commercial-block__video-title')['href']
+        title = entry.find('a', class_='commercial-block__video-title')['title']
+        brand = entry['data-advertiser']
+        air_time = entry['data-quarter']
 
-        title = entry.find('h2', class_='ranking_title').text.strip()
-
-        brand = entry.find('span', class_='ranking_advertiser').text
+        score = entry.find('dd', class_="average-score__num")
+        avg_ranking = score.text.strip()
 
         csvwriter.writerow([year, brand, title, index+1, avg_ranking, air_time, link])    
