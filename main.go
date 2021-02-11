86358ee1a8a8ff1c048a71d2c763e853c48eae91
Sam <sagottlieb@gmail.com>
2021-02-11 12:58:46 -0500

    Create Ad struct and use to unmarshal results from fql run. Verify that links are extracted; other fields not yet implemented.

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"os"
 
@@ -10,6 +11,14 @@ import (
 	"github.com/MontFerret/ferret/pkg/drivers/cdp"
 	"github.com/MontFerret/ferret/pkg/drivers/http"
 )
+
+type Ad struct {
+	Brand   string
+	Title   string
+	AvgRank float32
+	AirTime string
+	Link    string
+}
 
 var query = `
 LET doc = DOCUMENT("https://admeter.usatoday.com/results/2021")
@@ -33,10 +42,21 @@ func main() {
 	// site returns 404 with content of interest
 	ctx = drivers.WithContext(ctx, http.NewDriver(http.WithAllowedHTTPCode(404)), drivers.AsDefault())
 
-	_, err = program.Run(ctx)
+	out, err := program.Run(ctx)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
+	var results []*Ad
+	err = json.Unmarshal(out, &results)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for idx, ad := range results {
+		_ = idx
+		fmt.Println(ad.Link)
+	}
 }
